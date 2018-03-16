116034b0c9aff24dcf15142f690d44247a396ce9
Maciej Rzasa <maciej@textmaster.com>
2018-03-16 13:16:22 +0100

    removed rails_event_store_dependency

--- lib/rails_event_store_mongoid.rb
+++ lib/rails_event_store_mongoid.rb
@@ -1,6 +1,3 @@
-require 'rails_event_store/all'
 require 'rails_event_store_mongoid/event'
 require 'rails_event_store_mongoid/event_repository'
 require 'rails_event_store_mongoid/version'
-
-RailsEventStore.event_repository = RailsEventStoreMongoid::EventRepository.new
